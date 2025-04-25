01a7d40503781c304ac0b5ead2723664b3a4e912
Carter Salapka <carter.salapka.23@cnu.edu>
2025-04-25 12:49:55 -0400

    Program actually closes on exit now, needed to stop the timer

--- assignment04/lib/src/main/java/SpaceInvaders.java
+++ assignment04/lib/src/main/java/SpaceInvaders.java
@@ -53,9 +53,14 @@ public class SpaceInvaders extends JFrame {
 		var resume = game.add("Resume");
 		game.addSeparator();
 		var exit = game.add("Quit");
+		
+		Panel panel = new Panel();
+		add(panel);
+		
 		exit.addActionListener(e -> {
 			var result = JOptionPane.showConfirmDialog(this, "Do you want to exit?");
 			if (result == JOptionPane.OK_OPTION) {
+				panel.pauseGame();
 				dispose();
 			}
 		});
@@ -67,94 +72,19 @@ public class SpaceInvaders extends JFrame {
 			public void windowClosing(WindowEvent e) {
 				var result = JOptionPane.showConfirmDialog(SpaceInvaders.this, "Dare to Quit?");
 				if (result == JOptionPane.YES_OPTION) {
+					panel.pauseGame();
 					dispose();
 				}				
 			}
 		});
-		
-		Panel panel = new Panel();
-		add(panel);
+
 		
 		
-//		add(new JPanel() {
-//			Base b = new Base(250,380,100,100); //for testing
-//			Invader t = new InvaderTop(100,200,150,150);
-//			Invader bottom = new InvaderBottom(300,200,150,150);
-//			Invader m = new InvaderMiddle(100,400,150,150);
-//			Invader mys = new Mystery(20,300,150,150);
-//			
-//			private int x = 325;
-//			private int y = 430;
-//			boolean fired = false;
-//			boolean right = false;
-//			boolean left = false;
-//			{
-//				setBackground(Color.BLACK);
-//				setFocusable(true);
-//				addKeyListener( new KeyListener() {
-//					@Override
-//					public void keyTyped(KeyEvent e) {
-//					}
-//					@Override
-//					public void keyPressed(KeyEvent e) {
-//						var code = e.getKeyCode();
-//						if (code == KeyEvent.VK_LEFT) left = true;
-//						if (code == KeyEvent.VK_RIGHT) right = true;
-//						
-//						if (code == KeyEvent.VK_SPACE) {
-////							sound.setFramePosition(0);
-////							sound.start();
-//							fired = true;
-//						}
-//						repaint();
-//					}
-//					@Override
-//					public void keyReleased(KeyEvent e) {
-//						var code = e.getKeyCode();
-//						if (code == KeyEvent.VK_SPACE)
-////							sound.stop();
-//						if (code == KeyEvent.VK_LEFT) left = false;
-//						if (code == KeyEvent.VK_RIGHT) right = false;
-//					}
-//				});
-//				timer = new Timer(50, e -> {
-//					if (fired) {
-//						//rocket
-//					}
-//					if (right && x < 650) x += 20;
-//					if (left && x > 20) x -= 20;
-//					repaint();
-//					
-//				});
-//				timer.start();
-//			}
-//
-//			@Override
-//			protected void paintComponent(Graphics g) {
-//				super.paintComponent(g);
-//				var g2 = (Graphics2D) g;
-//				
-//				b.draw(g2); //for testing
-//				t.draw(g2);
-//				bottom.draw(g2);
-//				mys.draw(g2);
-//				m.draw(g2);
-////			}
-////				g2.drawImage(image, x, y, null);
-//			}
-//			
-//		});
-//		addWindowListener(new WindowAdapter() {
-//			@Override
-//			public void windowClosing(WindowEvent e) {
-//				timer.stop();
-//			}
-//		});
 		setSize(500, 500);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);	
-		//timer.stop();
+		
 	}
 	
 	public static void main(String[] args) {
