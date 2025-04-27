799a101a3fd887355fedcde1e8523ef3c4eb18f4
kpfarn <kevin.farnsworth.23@cnu.edu>
2025-04-27 07:41:06 -0400

    pulled and fixed name error on invaderB image

--- assignment04/lib/src/main/java/InvaderMiddle.java
+++ assignment04/lib/src/main/java/InvaderMiddle.java
@@ -9,6 +9,6 @@ public class InvaderMiddle extends Invader {
 	public InvaderMiddle(int x, int y, int w, int h) {
 		super(x,y,w,h,20);
 		normImage = Drawable.getImage("img_invadermiddleA.gif");
-		swapImage = Drawable.getImage("img_invadermiddleb.gif");
+		swapImage = Drawable.getImage("img_invadermiddleB.gif");
 	}
 }
